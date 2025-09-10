277dcd04b3273648a8a7115253fc36909318280b
simonhayes51 <simonhayes_@hotmail.com>
2025-09-10 07:58:27 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
-// src/main.jsx
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { EntitlementsProvider } from "./context/EntitlementsContext";
 
-import { EntitlementsProvider } from '@/context/EntitlementsContext'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <EntitlementsProvider>
       <App />
     </EntitlementsProvider>
   </React.StrictMode>
-)
+);
