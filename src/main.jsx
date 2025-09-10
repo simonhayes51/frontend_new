73932e8775237836468faa12d085a25c8d33ab87
simonhayes51 <simonhayes_@hotmail.com>
2025-09-10 08:25:08 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./index.css";
 import { EntitlementsProvider } from "./context/EntitlementsContext";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const container = document.getElementById("root");
+if (!container) {
+  throw new Error('Root element "#root" not found');
+}
+
+createRoot(container).render(
   <React.StrictMode>
     <EntitlementsProvider>
       <App />
