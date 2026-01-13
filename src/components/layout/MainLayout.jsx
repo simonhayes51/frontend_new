6aef9df5520ec3d57103f90901ec033ae72496aa
simonhayes51 <simonhayes_@hotmail.com>
2026-01-13 08:39:47 +0000

    fix: Add Outlet to MainLayout so routes render

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,14 +1,15 @@
+import { Outlet } from "react-router-dom";
 import { Sidebar } from "./Sidebar";
 import { TopBar } from "./TopBar";
 
-export function MainLayout({ children }) {
+export function MainLayout() {
   return (
     <div className="min-h-screen flex w-full bg-background">
       <Sidebar />
       <div className="flex-1 flex flex-col min-h-screen">
         <TopBar />
         <main className="flex-1 p-6 overflow-auto scrollbar-thin">
-          {children}
+          <Outlet />
         </main>
       </div>
     </div>
