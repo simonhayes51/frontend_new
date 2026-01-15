a1575b6c58904c46cccf5bc3ddcdc870ce32f4c0
simonhayes51 <simonhayes_@hotmail.com>
2026-01-15 19:11:34 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-
+  },
   server: {
     host: true,
     port: process.env.PORT ? parseInt(process.env.PORT, 10) : 5173,
